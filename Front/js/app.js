047771f1d5aabbcfec8218d66e20f1f02a4b65f1
MariemOi <mariem.ould-ismail@polytechnique.edu>
2023-10-30 00:51:13 +0100

    v29102023

--- Front/js/app.js
+++ Front/js/app.js
@@ -6,88 +6,38 @@ $(document).ready(function () {
     var hash = window.location.hash;
     switch (hash) {
 
-      case "#factures":
-        $.get("template/factures.tpl.html", function (template) {
-          $.getJSON("json/factures.json", function (data) {
-            var content = Mustache.render(template, data);
-            $("#specific_content").html(content);
-          });
-        }, "text");
+      case "#facturevendeur":
+        $.get("template/facturevendeur.tpl.html", function (template) {
+          $("#my-content").html(template);
+        }, "html");
         break;
-
-      case "calendrier":
-        $.get("template/calendrier.tpl.html", function (template) {
-          $("#specific_content").html(template);
-        }, "text");
-
-
-      case "#commandes":
-        $.get("template/commandes.tpl.html", function (template) {
-          $.getJSON("json/commandes.json", function (data) {
-            var content = Mustache.render(template, data);
-            $("#specific_content").html(content);
-          });
-        }, "text");
+        case "#commandevendeur":
+        $.get("template/commandevendeur.tpl.html", function (template) {
+          $("#my-content").html(template);
+        }, "html");
         break;
-
-      case "#feed":
-        $.get("template/feed.tpl.html", function (template) {
-          $("#specific_content").html(template);
-        }, "text");
-
-        $.get("template/post.tpl.html", function (template) {
-          $.getJSON("json/posts.json", function (data) {
-            var content = Mustache.render(template, data);
-            $("#fil-actu").html(content);
-          });
-        }, "text");
-
+        
+      case "#stock":
+        $.get("template/stock.tpl.html", function (template) {
+          $("#my-content").html(template);
+        }, "html");
         break;
-
-      case "#order":
-
-        $.get("template/order.tpl.html", function (template) {
-          $.getJSON("json/post" + sessionStorage['post_id'] + ".json", function (data) {
-            var content = Mustache.render(template, data);
-            $("#specific_content").html(content);
-          });
-        }, "text");
-
+      case "#post":
+        $.get("template/feedvendeur.tpl.html", function (template) {
+          $("#my-content").html(template);
+        }, "html");
         break;
-
-      case "#discover": /* Comment faire en sorte que le template ne se charge que lorsque le precedent est charge */
-        $.get("template/discover.tpl.html", function (template) {
-          $("#specific_content").html(template);
-        }, "text");
-
-        $.get("template/discover-vendeurs.tpl.html", function (template) {
-          $.getJSON("json/vendeurs.json", function (data) {
-            var content = Mustache.render(template, data);
-            $("#discover-vendeurs").html(content);
-          });
-        }, "text");
-
-        $.get("template/discover-reels.tpl.html", function (template) {
-          $.getJSON("json/reels.json", function (data) {
-            var content = Mustache.render(template, data);
-            $("#discover-reels").html(content);
-          });
-        }, "text");
-
-        $.get("template/discover-posts.tpl.html", function (template) {
-          $.getJSON("json/newposts.json", function (data) {
-            var content = Mustache.render(template, data);
-            $("#discover-posts").html(content);
-          });
-        }, "text");
-        $.get("template/post.tpl.html", function (template) {
-          $.getJSON("json/newposts.json", function (data) {
-            var content = Mustache.render(template, data);
-            $("#new_posts").html(content);
-          });
-        }, "text");
-
+      case "#conversationvendeur":
+        $.get("template/messagerievendeur.tpl.html", function (template) {
+          $("#my-content").html(template);
+        }, "html");
         break;
+      
+  
+      default:
+        $.get("template/indexvendeur.tpl.html", function (template) {
+          $("#my-content").html(template);
+        }, "html");
     }
   }
 
